514898b499220c14b6680a5a1c921444e5955b86
faeze-73 <faeze.fallahnezhad73@gmail.com>
2022-05-15 21:24:07 +0430

    add some pages

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <my-header></my-header>
+    <!-- Page content -->
+  <div class="container">
+    <div class="row">
+      <!-- Blog entries -->
+      <post-list></post-list>
+      <my-sidebar></my-sidebar>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./../layouts/Header.vue";
+import PostList from "./../PostList.vue";
+import Sidebar from "./../layouts/Sidebar.vue";
+export default {
+  components: {
+    "my-header": Header,
+    "my-sidebar": Sidebar,
+    PostList
+  } 
+}
+</script>
+
+<style>
+
+</style>
